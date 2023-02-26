7b4015bc0d32761c7fdfeca4e6bcb39643f71a85
Tony Bi <tonnybida@gmail.com>
2023-02-26 23:12:23 +1300

    Change file path

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import './App.css'
 import Login from './components/Login/Login'
-import PageContainer from './UI/Card/PageContainer/PageContainer'
 
 const App = () => {
     return (
         <div className="App">
-            <PageContainer>
-                <Login />
-            </PageContainer>
+            <Login />
         </div>
     )
 }
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,11 +1,14 @@
+import PageContainer from '../../UI/PageContainer/PageContainer'
 import Card from '../../UI/Card/Card'
 import style from './Login.module.css'
 
 const Login = () => {
     return (
-        <Card className={style.login}>
-            <form action="">this is login form</form>
-        </Card>
+        <PageContainer>
+            <Card className={style.login}>
+                <form action="">this is login form</form>
+            </Card>
+        </PageContainer>
     )
 }
 
